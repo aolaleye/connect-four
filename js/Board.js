a2953c1b01d92e6f6efcf51918bb02e441ffaf45
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-09-20 16:00:26 -0400

    created constructors

--- js/Board.js
+++ js/Board.js
@@ -1,3 +1,13 @@
 class Board {
-    
+    constructor () {
+        this.rows = 6;
+        this.columns = 7;
+        this.spaces = [];
+    }
+
+    /** 
+     * Generates 2D array of spaces. 
+     * @return  {Array}     An array of space objects
+     */
+    createSpaces() {}
 }
