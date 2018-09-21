e77ab51ff6f6adf505b6bd4240a2c0a00cb20c21
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-09-21 10:33:15 -0400

    created method to add players and spaces

--- js/Board.js
+++ js/Board.js
@@ -2,12 +2,27 @@ class Board {
     constructor () {
         this.rows = 6;
         this.columns = 7;
-        this.spaces = [];
+        this.spaces = this.createSpaces();
     }
 
     /** 
      * Generates 2D array of spaces. 
      * @return  {Array}     An array of space objects
      */
-    createSpaces() {}
+    createSpaces() {
+        const spaces = [];
+
+        for (i = 0; i < this.columns; i++) {
+            const column = [];
+
+            for (j = 0; i < this.rows; j++) {
+                const space = new Space(x, y);
+                column.push(space);
+            } 
+
+            spaces.push(column);
+        } 
+
+        return spaces;
+    }
 }
